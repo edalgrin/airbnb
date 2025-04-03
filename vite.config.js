ecb3ee122bedc66555fc819d614985a338dd3356
edalgrin <allegrini.eduardo@gmail.com>
2025-04-03 11:31:18 +0200

    Fix path issue in build

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: "./",
   build: {
     outDir: "./docs",
-    emptyOutDir: true, // also necessary
+    emptyOutDir: true,
   },
 });
