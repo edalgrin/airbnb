117c047a346545fc979661ee5988bdddf4aa2042
edalgrin <allegrini.eduardo@gmail.com>
2025-04-03 11:05:14 +0200

    Change dist folder

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    outDir: "./docs",
+    emptyOutDir: true, // also necessary
+  },
 });
