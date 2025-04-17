5b7ca034e16dccd1ed747bf62fb8301962b1cd28
edalgrin <allegrini.eduardo@gmail.com>
2025-04-17 19:36:30 +0200

    Add image carousel

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import classNames from "classnames";
 import {
   IoStar,
@@ -8,9 +8,7 @@ import {
   IoArrowBack,
 } from "react-icons/io5";
 
-const handleSlideClick = () => {
-  alert("Slide the carousel to show more images");
-};
+import useEmblaCarousel from "embla-carousel-react";
 
 const Card = ({
   id,
@@ -25,6 +23,39 @@ const Card = ({
 }) => {
   const [bookmarked, setBookmarked] = useState(bookmark);
   const [card, setCard] = useState(true);
+  const [canScrollPrev, setCanScrollPrev] = useState(false);
+  const [canScrollNext, setCanScrollNext] = useState(true);
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  const scrollTo = useCallback(
+    (index) => {
+      if (emblaApi) emblaApi.scrollTo(index);
+    },
+    [emblaApi]
+  );
+
+  const onSelect = useCallback(() => {
+    if (!emblaApi) return;
+    setCanScrollPrev(emblaApi.canScrollPrev());
+    setCanScrollNext(emblaApi.canScrollNext());
+  }, [emblaApi]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onSelect();
+    emblaApi.on("select", onSelect);
+
+    return () => emblaApi.off("select", onSelect);
+  }, [emblaApi, onSelect]);
 
   useEffect(() => {
     setTimeout(() => {
@@ -37,7 +68,7 @@ const Card = ({
       className={classNames("relative text-sm", { card: card })}
       style={{ "--delay": `${id}00ms` }}
     >
-      <div className="flex flex-row-reverse justify-between items-center absolute top-3 left-3 right-3">
+      <div className="flex flex-row-reverse justify-between items-center absolute z-1 top-3 left-3 right-3">
         <button
           aria-label="Favorite"
           className="btn-2 rounded-full p-3 text-xl z-1"
@@ -68,16 +99,33 @@ const Card = ({
           </span>
         )}
       </div>
-      <div className="aspect-square overflow-hidden">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            alt=""
-            src={`${image}/300.jpg`}
-            loading="lazy"
-            className={classNames("rounded-xl w-full", { hidden: index != 0 })}
-          />
-        ))}
+      <div
+        className="embla aspect-square overflow-hidden relative"
+        ref={emblaRef}
+      >
+        <div className="embla__container flex">
+          {images.map((image, index) => (
+            <img
+              key={index}
+              alt=""
+              src={`${image}/300.jpg`}
+              loading="lazy"
+              className={classNames("embla__slide rounded-xl w-full", {})}
+            />
+          ))}
+        </div>
+
+        <div className="embla__dots flex gap-1 justify-center mt-2 absolute bottom-2 left-0 right-0 z-1">
+          {images.map((image, index) => (
+            <button
+              key={index}
+              onClick={() => scrollTo(index)}
+              className={classNames("p-1 rounded-full btn-2", {
+                "bg-gray-800": emblaApi?.selectedScrollSnap() === index,
+              })}
+            ></button>
+          ))}
+        </div>
       </div>
       <div className="flex justify-between items-center gap-3 mt-2">
         <div className="font-semibold">{title}</div>
@@ -95,7 +143,7 @@ const Card = ({
       <a
         href={`#more${id}`}
         aria-label="View more"
-        className="hidden group rounded-lg before:absolute before:inset-0 before:rounded-xl before:content-['']"
+        className="group peer rounded-lg before:absolute before:inset-0 before:rounded-xl before:content-['']"
       >
         <span
           aria-hidden="true"
@@ -103,31 +151,27 @@ const Card = ({
         >
           View more
         </span>
-
-        {images.length > 1 && (
-          <>
-            <button
-              aria-label="Previous Image"
-              onClick={() => {
-                handleSlideClick();
-              }}
-              className="btn-2 rounded-full p-3 text-xl absolute top-[33%] left-3 opacity-0"
-            >
-              <IoArrowBack aria-hidden="true" />
-            </button>
-
-            <button
-              aria-label="Next Image"
-              onClick={() => {
-                handleSlideClick();
-              }}
-              className="btn-2 rounded-full p-3 text-xl absolute top-[33%] right-3 opacity-0 group-hover:opacity-100 focus:opacity-100 duration-300 ease-in-out"
-            >
-              <IoArrowForward aria-hidden="true" />
-            </button>
-          </>
-        )}
       </a>
+
+      {canScrollPrev && (
+        <button
+          aria-label="Previous Image"
+          onClick={scrollPrev}
+          className="embla__prev btn-2"
+        >
+          <IoArrowBack aria-hidden="true" />
+        </button>
+      )}
+
+      {canScrollNext && (
+        <button
+          aria-label="Next Image"
+          onClick={scrollNext}
+          className="embla__next btn-2"
+        >
+          <IoArrowForward aria-hidden="true" />
+        </button>
+      )}
     </div>
   );
 };
